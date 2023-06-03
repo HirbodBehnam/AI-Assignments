084f79f1063c0a93af7878384bd7bb5f511657c2
Hirbod Behnam <chrome.hiri.angry@gmail.com>
2023-06-03 19:41:09 +0330

    Started HW7

--- 6/Practical/2.py
+++ 6/Practical/2.py
@@ -3,22 +3,6 @@ TRANSITION_PROBS = [list(map(float, input().split())) for _ in range(3)]
 EMISSION_PROBS = [list(map(float, input().split())) for _ in range(3)]
 EVIDENCE = list(map(int, input().split()))
 
-current_probs = INITIAL_PROBS.copy()
-best_probs = []
-for i in range(len(current_probs)):
-    current_probs[i] *= EMISSION_PROBS[i][EVIDENCE[0]]
-#print(current_probs)
-best_probs.append(current_probs.index(max(current_probs)))
-for evidence in EVIDENCE[1:]:
-    new_probs = [0, 0, 0]
-    for i in range(len(new_probs)):
-        for (index, prob) in enumerate(current_probs): # forward
-            new_probs[i] += prob * TRANSITION_PROBS[index][i]
-        new_probs[i] *= EMISSION_PROBS[i][evidence]
-    #print(new_probs)
-    current_probs = new_probs
-    best_probs.append(current_probs.index(max(current_probs)))
-
 def num_to_temp(n: int) -> str:
     if n == 0:
         return "cold"
@@ -27,9 +11,48 @@ def num_to_temp(n: int) -> str:
     if n == 2:
         return "hot"
     return "duck"
-prob_sum = sum(current_probs)
-current_probs = list(map(lambda x: round(x / prob_sum, 3), current_probs)) # normalize
-print("Most likely sequence of states:")
-print(list(map(num_to_temp, best_probs)))
-print("Probability of states after observing:")
-print(current_probs)
+
+def forward_algorithm():
+    current_probs = INITIAL_PROBS.copy()
+    best_probs = []
+    for i in range(len(current_probs)):
+        current_probs[i] *= EMISSION_PROBS[i][EVIDENCE[0]]
+    #print(current_probs)
+    best_probs.append(current_probs.index(max(current_probs)))
+    for evidence in EVIDENCE[1:]:
+        new_probs = [0, 0, 0]
+        for i in range(len(new_probs)):
+            for (index, prob) in enumerate(current_probs):
+                new_probs[i] += prob * TRANSITION_PROBS[index][i]
+            new_probs[i] *= EMISSION_PROBS[i][evidence]
+        #print(new_probs)
+        current_probs = new_probs
+        best_probs.append(current_probs.index(max(current_probs)))
+
+    prob_sum = sum(current_probs)
+    current_probs = list(map(lambda x: round(x / prob_sum, 3), current_probs)) # normalize
+    print("Probability of states after observing:")
+    print(current_probs)
+
+def viterbi_algorithm():
+    current_probs = INITIAL_PROBS.copy()
+    best_probs = []
+    for i in range(len(current_probs)):
+        current_probs[i] *= EMISSION_PROBS[i][EVIDENCE[0]]
+    best_probs.append(current_probs.index(max(current_probs)))
+    for evidence in EVIDENCE[1:]:
+        new_probs = [0, 0, 0]
+        for i in range(len(new_probs)):
+            for (index, prob) in enumerate(current_probs):
+                new_probs[i] = max(new_probs[i], prob * TRANSITION_PROBS[index][i])
+            new_probs[i] *= EMISSION_PROBS[i][evidence]
+        #print(new_probs)
+        current_probs = new_probs
+        best_probs.append(current_probs.index(max(current_probs)))
+
+    print("Most likely sequence of states:")
+    print(list(map(num_to_temp, best_probs)))
+
+
+viterbi_algorithm()
+forward_algorithm()
